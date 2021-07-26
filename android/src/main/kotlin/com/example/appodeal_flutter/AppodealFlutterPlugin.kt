9c1444412df1e67c56fe1ecb9731aa7573dbc3fd
KulArtsiom <kulartsiom@gmail.com>
2021-07-26 16:05:02 +0300

    Update AppodealFlutterPlugin.kt

--- android/src/main/kotlin/com/example/appodeal_flutter/AppodealFlutterPlugin.kt
+++ android/src/main/kotlin/com/example/appodeal_flutter/AppodealFlutterPlugin.kt
@@ -1,13 +1,16 @@
 package com.example.appodeal_flutter
 
+import android.app.Activity
 import androidx.annotation.NonNull
-
+import com.appodeal.ads.Appodeal
+import com.appodeal.ads.utils.Log
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** AppodealFlutterPlugin */
 class AppodealFlutterPlugin: FlutterPlugin, MethodCallHandler {
@@ -28,9 +31,55 @@ class AppodealFlutterPlugin: FlutterPlugin, MethodCallHandler {
     } else {
       result.notImplemented()
     }
+
+    when (call.method) {
+      "setTesting" -> setTesting(call, result)
+      "getPlatformVersion" -> getPlatformVersion(call, result)
+      "setAutoCache" -> setAutoCache(call, result)
+      else -> result.notImplemented()
+    }
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
+
+  private fun setTesting(call: MethodCall, result: Result) {
+    val args = call.arguments as Map<*, *>
+    val value = args["testMode"] as Boolean
+    if (value){
+      Appodeal.setTesting(value)
+    }
+    result.success(null)
+  }
+
+  private fun setAutoCache(call: MethodCall, result: Result) {
+    val args = call.arguments as Map<*, *>
+    val adType = getAdType(args["adType"] as Int)
+    val autoCache = args["autoCache"] as Boolean
+
+    Appodeal.setAutoCache(adType, autoCache)
+    result.success(null)
+  }
+
+  private fun getPlatformVersion(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  private fun getAdType(adId: Int): Int {
+    return when (adId) {
+      1 -> Appodeal.BANNER
+      2 -> Appodeal.NATIVE
+      3 -> Appodeal.INTERSTITIAL
+      4 -> Appodeal.REWARDED_VIDEO
+      5 -> Appodeal.NON_SKIPPABLE_VIDEO
+      else -> Appodeal.NONE
+    }
+  }
+
+
 }
