366aa637580c4a69dac58793063d111e99f25932
KulArtsiom <kulartsiom@gmail.com>
2021-07-26 16:39:28 +0300

    Update appodeal.dart

--- lib/src/appodeal.dart
+++ lib/src/appodeal.dart
@@ -11,6 +11,10 @@ class Appodeal {
   static const REWARD = 4;
   static const NON_SKIPPABLE = 5;
 
+  static const LogLevelNone = 0;
+  static const LogLevelDebug = 1;
+  static const LogLevelVerbose = 2;
+
   static const MethodChannel _channel =
   const MethodChannel('appodeal_flutter');
 
@@ -22,6 +26,12 @@ class Appodeal {
   static Future<void> setTesting(bool testMode) async {
     return _channel.invokeMethod('setTesting', {
       'testMode': testMode,
+    });
+  }
+
+  static Future<void> setLogLevel(int logLevel) async {
+    return _channel.invokeMethod('setLogLevel', {
+      'logLevel': logLevel,
     });
   }
 
