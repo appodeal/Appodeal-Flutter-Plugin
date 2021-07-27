50566b0dc4f9a3f703538a7d53f56a10d93a5e65
KulArtsiom <kulartsiom@gmail.com>
2021-07-27 16:40:56 +0300

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:appodeal_flutter/appodeal_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  String androidAppKey = "fee50c333ff3825fd6ad6d38cff78154de3025546d47a84f";
 
   @override
   void initState() {
@@ -34,8 +36,9 @@ class _MyAppState extends State<MyApp> {
 
     Appodeal.setLogLevel(Appodeal.LogLevelVerbose);
     Appodeal.setAutoCache(Appodeal.BANNER, true);
+    Appodeal.setAutoCache(Appodeal.INTERSTITIAL, false);
+    Appodeal.setAutoCache(Appodeal.REWARDED_VIDEO, true);
     Appodeal.setTesting(true);
-
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
@@ -52,11 +55,69 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          centerTitle: true,
+          title: const Text('Appodeal Flutter Demo'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+        body: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                onPressed: () {
+                  Appodeal.initialize(androidAppKey, [Appodeal.REWARDED_VIDEO, Appodeal.INTERSTITIAL, Appodeal.BANNER], false);
+                },
+                child: const Text('Initialize'),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                onPressed: () async {
+                  var isInitialized = await Appodeal.isInitialized(Appodeal.INTERSTITIAL);
+                  Fluttertoast.showToast(msg: '[AF Plugin]: Appodeal.isInitialized(Appodeal.INTERSTITIAL) - $isInitialized',
+                      toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM, timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.red, textColor: Colors.white, fontSize: 16.0);
+                },
+                child: const Text('isInitialized Interstitial'),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                onPressed: () async {
+                  var isInitialized = await Appodeal.isAutoCacheEnabled(Appodeal.INTERSTITIAL);
+                  Fluttertoast.showToast(msg: '[AF Plugin]: Appodeal.isAutoCacheEnabled(Appodeal.INTERSTITIAL) - $isInitialized',
+                      toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM, timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.red, textColor: Colors.white, fontSize: 16.0);
+                  Appodeal.updateConsent(true);
+                },
+                child: const Text('isAutoCacheEnabled Interstitial'),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                onPressed: () async {
+                  Appodeal.cache(Appodeal.INTERSTITIAL);
+                },
+                child: const Text('Cache Interstitial'),
+              ),
+            ],
+          ),
+          Center(
+            child: Text('Running on: $_platformVersion\n'),
+          ),
+        ]),
       ),
     );
   }
