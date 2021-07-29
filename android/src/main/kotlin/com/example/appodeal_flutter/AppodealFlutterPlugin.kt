35099852d59dc2d210ead87240c3544b6e8bed08
KulArtsiom <kulartsiom@gmail.com>
2021-07-29 17:30:59 +0300

    Update AppodealFlutterPlugin.kt

--- android/src/main/kotlin/com/example/appodeal_flutter/AppodealFlutterPlugin.kt
+++ android/src/main/kotlin/com/example/appodeal_flutter/AppodealFlutterPlugin.kt
@@ -3,6 +3,7 @@ package com.example.appodeal_flutter
 import android.app.Activity
 import androidx.annotation.NonNull
 import com.appodeal.ads.Appodeal
+import com.appodeal.ads.UserSettings
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -38,11 +39,25 @@ class AppodealFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
             "isLoaded" -> isLoaded(call, result)
             "isPrecache" -> isPrecache(call, result)
             "setSmartBanners" -> setSmartBanners(call, result)
-
-
+            "setTabletBanners" -> setTabletBanners(call, result)
+            "setBannerAnimation" -> setBannerAnimation(call, result)
+            "setBannerRotation" -> setBannerRotation(call, result)
+            "trackInAppPurchase" -> trackInAppPurchase(call, result)
+            "disableNetwork" -> disableNetwork(call, result)
+            "disableNetworkForSpecificAdType" -> disableNetworkForSpecificAdType(call, result)
+            "disableLocationPermissionCheck" -> disableLocationPermissionCheck(result)
+            "disableWriteExternalStoragePermissionCheck" -> disableWriteExternalStoragePermissionCheck(result)
+            "setUserId" -> setUserId(call, result)
+            "setUserAge" -> setUserAge(call, result)
+            "setUserGender" -> setUserGender(call, result)
             "setTesting" -> setTesting(call, result)
-            "setAutoCache" -> setAutoCache(call, result)
             "setLogLevel" -> setLogLevel(call, result)
+            "setCustomFilter" -> setCustomFilter(call, result)
+            "setCustomFilterInt" -> setCustomFilterInt(call, result)
+            "setCustomFilterDouble" -> setCustomFilterDouble(call, result)
+            "setCustomFilterBool" -> setCustomFilterBool(call, result)
+
+            "setAutoCache" -> setAutoCache(call, result)
 
             else -> result.notImplemented()
         }
@@ -169,7 +184,7 @@ class AppodealFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         val args = call.arguments as Map<*, *>
         val adType = getAdType(args["adType"] as Int)
         val onLoadedTriggerBoth = args["onLoadedTriggerBoth"] as Boolean
-        Appodeal.setTriggerOnLoadedOnPrecache(adType,onLoadedTriggerBoth)
+        Appodeal.setTriggerOnLoadedOnPrecache(adType, onLoadedTriggerBoth)
         result.success(null)
     }
 
@@ -196,6 +211,117 @@ class AppodealFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         val args = call.arguments as Map<*, *>
         val smartBannerEnabled = args["smartBannerEnabled"] as Boolean
         Appodeal.setSmartBanners(smartBannerEnabled)
+        result.success(null)
+    }
+
+    private fun setTabletBanners(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val tabletBannerEnabled = args["tabletBannerEnabled"] as Boolean
+        Appodeal.set728x90Banners(tabletBannerEnabled)
+        result.success(null)
+    }
+
+    private fun setBannerAnimation(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val bannerAnimationEnabled = args["bannerAnimationEnabled"] as Boolean
+        Appodeal.setBannerAnimation(bannerAnimationEnabled)
+        result.success(null)
+    }
+
+    private fun setBannerRotation(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val leftBannerRotation = getAdType(args["leftBannerRotation"] as Int)
+        val rightBannerRotation = getAdType(args["rightBannerRotation"] as Int)
+        Appodeal.setBannerRotation(leftBannerRotation, rightBannerRotation)
+        result.success(null)
+    }
+
+    private fun trackInAppPurchase(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val amount = args["amount"] as Double
+        val currency = args["currency"] as String
+        Appodeal.trackInAppPurchase(activity, amount, currency)
+        result.success(null)
+    }
+
+    private fun disableNetwork(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val network = args["network"] as String
+        Appodeal.disableNetwork(activity, network);
+        result.success(null)
+    }
+
+    private fun disableNetworkForSpecificAdType(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val network = args["network"] as String
+        val adType = getAdType(args["adType"] as Int)
+        Appodeal.disableNetwork(activity, network, adType);
+        result.success(null)
+    }
+
+    private fun disableLocationPermissionCheck(result: Result) {
+        Appodeal.disableLocationPermissionCheck()
+        result.success(null)
+    }
+
+    private fun disableWriteExternalStoragePermissionCheck(result: Result) {
+        Appodeal.disableWriteExternalStoragePermissionCheck()
+        result.success(null)
+    }
+
+    private fun setUserId(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val userId = args["userId"] as String
+        Appodeal.setUserId(userId);
+        result.success(null)
+    }
+
+    private fun setUserAge(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val age = args["age"] as Int
+        Appodeal.setUserAge(age);
+        result.success(null)
+    }
+
+    private fun setUserGender(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        when (args["gender"] as Int) {
+            0 -> Appodeal.setUserGender(UserSettings.Gender.OTHER)
+            1 -> Appodeal.setUserGender(UserSettings.Gender.MALE)
+            2 -> Appodeal.setUserGender(UserSettings.Gender.FEMALE)
+        }
+        result.success(null)
+    }
+
+    private fun setCustomFilter(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val name = args["name"] as String
+        val value = args["value"] as String
+        Appodeal.setCustomFilter(name, value)
+        result.success(null)
+    }
+
+    private fun setCustomFilterBool(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val name = args["name"] as String
+        val value = args["value"] as Boolean
+        Appodeal.setCustomFilter(name, value)
+        result.success(null)
+    }
+
+    private fun setCustomFilterInt(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val name = args["name"] as String
+        val value = args["value"] as Int
+        Appodeal.setCustomFilter(name, value)
+        result.success(null)
+    }
+
+    private fun setCustomFilterDouble(call: MethodCall, result: Result) {
+        val args = call.arguments as Map<*, *>
+        val name = args["name"] as String
+        val value = args["value"] as Double
+        Appodeal.setCustomFilter(name, value)
         result.success(null)
     }
 
