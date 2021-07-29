b4eceeef38e34cab8113f29dd2b6f10d0f03261e
KulArtsiom <kulartsiom@gmail.com>
2021-07-29 13:43:05 +0300

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:appodeal_flutter/appodeal_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
@@ -15,7 +14,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
   String androidAppKey = "fee50c333ff3825fd6ad6d38cff78154de3025546d47a84f";
 
   @override
@@ -26,28 +24,21 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Appodeal.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
 
     Appodeal.setLogLevel(Appodeal.LogLevelVerbose);
     Appodeal.setAutoCache(Appodeal.BANNER, true);
-    Appodeal.setAutoCache(Appodeal.INTERSTITIAL, false);
+    Appodeal.setAutoCache(Appodeal.INTERSTITIAL, true);
     Appodeal.setAutoCache(Appodeal.REWARDED_VIDEO, true);
+    Appodeal.setTriggerOnLoadedOnPrecache(Appodeal.INTERSTITIAL, true);
+    Appodeal.setSharedAdsInstanceAcrossActivities(true);
     Appodeal.setTesting(true);
+    Appodeal.setSmartBanners(false);
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
     if (!mounted) return;
 
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -71,52 +62,169 @@ class _MyAppState extends State<MyApp> {
               ),
             ],
           ),
-          Row(
+          //Interstitial
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
-                onPressed: () async {
-                  var isInitialized = await Appodeal.isInitialized(Appodeal.INTERSTITIAL);
-                  Fluttertoast.showToast(msg: '[AF Plugin]: Appodeal.isInitialized(Appodeal.INTERSTITIAL) - $isInitialized',
-                      toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM, timeInSecForIosWeb: 1,
-                      backgroundColor: Colors.red, textColor: Colors.white, fontSize: 16.0);
-                },
-                child: const Text('isInitialized Interstitial'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(child: Text('Interstitial')),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                      onPressed: () async {
+                        var isInitialized = await Appodeal.isInitialized(Appodeal.INTERSTITIAL);
+                        Fluttertoast.showToast(msg: 'isInitialized Interstitial - $isInitialized', toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM, timeInSecForIosWeb: 1, backgroundColor: Colors.red, textColor: Colors.white, fontSize: 16.0);
+                      },
+                      child: const Text('isInitialized'),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                      onPressed: () async {
+                        var isLoaded = await Appodeal.isLoaded(Appodeal.INTERSTITIAL);
+                        Fluttertoast.showToast(msg: 'isLoaded Interstitial - $isLoaded',
+                            toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM,
+                            timeInSecForIosWeb: 1, backgroundColor: Colors.red,
+                            textColor: Colors.white, fontSize: 16.0);
+                      },
+                      child: const Text('isLoaded'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                      onPressed: () async {
+                      await Appodeal.showWithPlacement(Appodeal.INTERSTITIAL, "default");
+                      },
+                      child: const Text('show'),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-          Row(
+          //Rewarded video
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
-                onPressed: () async {
-                  var isInitialized = await Appodeal.isAutoCacheEnabled(Appodeal.INTERSTITIAL);
-                  Fluttertoast.showToast(msg: '[AF Plugin]: Appodeal.isAutoCacheEnabled(Appodeal.INTERSTITIAL) - $isInitialized',
-                      toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM, timeInSecForIosWeb: 1,
-                      backgroundColor: Colors.red, textColor: Colors.white, fontSize: 16.0);
-                  Appodeal.updateConsent(true);
-                },
-                child: const Text('isAutoCacheEnabled Interstitial'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(child: Text('Rewarded video')),
+                ],
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                      onPressed: () async {
+                        var isInitialized = await Appodeal.isInitialized(Appodeal.REWARDED_VIDEO);
+                        Fluttertoast.showToast(msg: 'isInitialized Rewarded video - $isInitialized',
+                            toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM,
+                            timeInSecForIosWeb: 1, backgroundColor: Colors.red,
+                            textColor: Colors.white, fontSize: 16.0);
+                      },
+                      child: const Text('isInitialized'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                      onPressed: () async {
+                        var isLoaded = await Appodeal.isLoaded(Appodeal.REWARDED_VIDEO);
+                        Fluttertoast.showToast(msg: 'isLoaded Rewarded video - $isLoaded',
+                            toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM,
+                            timeInSecForIosWeb: 1, backgroundColor: Colors.red,
+                            textColor: Colors.white, fontSize: 16.0);
+                      },
+                      child: const Text('isLoaded'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                      onPressed: () async {
+                        await Appodeal.show(Appodeal.REWARDED_VIDEO);
+                      },
+                      child: const Text('show'),
+                    ),
+                  ),
+                ],
+              ),
+
             ],
           ),
-          Row(
+          //Banner
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
-                onPressed: () async {
-                  Appodeal.cache(Appodeal.INTERSTITIAL);
-                },
-                child: const Text('Cache Interstitial'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(child: Text('Banner')),
+                ],
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                      onPressed: () async {
+                        var isInitialized = await Appodeal.isInitialized(Appodeal.BANNER);
+                        Fluttertoast.showToast(msg: 'isInitialized - $isInitialized',
+                            toastLength: Toast.LENGTH_SHORT, gravity: ToastGravity.BOTTOM,
+                            timeInSecForIosWeb: 1, backgroundColor: Colors.red,
+                            textColor: Colors.white, fontSize: 16.0);
+                      },
+                      child: const Text('isInitialized'),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                      onPressed: () async {
+                        Appodeal.show(Appodeal.BANNER_BOTTOM);
+                      },
+                      child: const Text('show'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20)),
+                      onPressed: () async {
+                        await Appodeal.hide(Appodeal.BANNER);
+                      },
+                      child: const Text('hide'),
+                    ),
+                  ),
+                ],
+              ),
+
             ],
           ),
-          Center(
-            child: Text('Running on: $_platformVersion\n'),
-          ),
+
         ]),
       ),
     );
