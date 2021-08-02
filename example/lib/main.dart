072854777632595cca3159b2189d18853d62a3e5
KulArtsiom <kulartsiom@gmail.com>
2021-08-02 19:58:29 +0300

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -58,6 +58,13 @@ class _AppodealDemoAppState extends State<AppodealDemoApp> {
 
     Appodeal.setUseSafeArea(true);
 
+    Appodeal.setRewardedVideoCallbacks();
+
+    Appodeal.videoListener =
+        (RewardedVideoAdEvent event, {String rewardType, int rewardAmount}) {
+          print("RewardedVideoAd event $event");
+        };
+
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
