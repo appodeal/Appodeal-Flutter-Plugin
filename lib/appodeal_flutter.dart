0fc4ec5af2037fe89ba642bc441e538a357dfa34
KulArtsiom <kulartsiom@gmail.com>
2021-08-10 13:06:52 +0300

    Update appodeal_flutter.dart

--- lib/appodeal_flutter.dart
+++ lib/appodeal_flutter.dart
@@ -1,13 +1,1 @@
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class AppodealFlutter {
-  static const MethodChannel _channel =
-      const MethodChannel('appodeal_flutter');
-
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'src/appodeal.dart';
