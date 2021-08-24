e7e4d91bfd308716c64e389e3de1665ae4c78fac
KulArtsiom <kulartsiom@gmail.com>
2021-08-24 11:39:56 +0300

    Update AppodealInterstitialDelegate.swift

--- ios/Classes/AppodealInterstitialDelegate.swift
+++ ios/Classes/AppodealInterstitialDelegate.swift
@@ -11,32 +11,32 @@ import Appodeal
 extension SwiftAppodealFlutterPlugin: AppodealInterstitialDelegate {
     public func interstitialDidLoadAdIsPrecache(_ precache: Bool) {
         let args: [String: Any] = [
-                "isPrecache": precache
-            ]
+            "isPrecache": precache
+        ]
         channel?.invokeMethod("onInterstitialLoaded", arguments: args)
         
     }
-
+    
     public func interstitialDidFailToLoadAd() {
         channel?.invokeMethod("onInterstitialFailedToLoad", arguments: nil)
     }
-
+    
     public func interstitialWillPresent() {
         channel?.invokeMethod("onInterstitialShown", arguments: nil)
     }
-
+    
     public func interstitialDidFailToPresent() {
         channel?.invokeMethod("onInterstitialShowFailed", arguments: nil)
     }
-
+    
     public func interstitialDidClick() {
         channel?.invokeMethod("onInterstitialClicked", arguments: nil)
     }
-
+    
     public func interstitialDidDismiss() {
         channel?.invokeMethod("onInterstitialClosed", arguments: nil)
     }
-
+    
     public func interstitialDidExpired() {
         channel?.invokeMethod("onInterstitialExpired", arguments: nil)
     }
