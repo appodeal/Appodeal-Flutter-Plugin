db793740f5bc5c776a9e3e5837d29bcac09ca4f2
KulArtsiom <kulartsiom@gmail.com>
2021-08-24 13:00:00 +0300

    Create AppodealRewardedVideoDelegate.swift

---
+++ ios/Classes/AppodealRewardedVideoDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  AppodealRewardedVideoDelegate.swift
+//  appodeal_flutter
+//
+//  Created by Artyom Kul on 24.08.21.
+//
+
+import Foundation
+import Appodeal
+
+extension SwiftAppodealFlutterPlugin: AppodealRewardedVideoDelegate {
+    
+    public func rewardedVideoDidLoadAdIsPrecache(_ precache: Bool) {
+        let args: [String: Any] = [
+            "isPrecache": precache
+        ]
+        channel?.invokeMethod("onRewardedVideoLoaded", arguments: args)
+    }
+    
+    
+    public  func rewardedVideoDidFailToLoadAd() {
+        channel?.invokeMethod("onRewardedVideoFailedToLoad", arguments: nil)
+    }
+    
+    public func rewardedVideoDidFailToPresentWithError(_ error: Error) {
+        channel?.invokeMethod("onRewardedVideoShowFailed", arguments: nil)
+    }
+    
+    
+    public   func rewardedVideoDidPresent() {
+        channel?.invokeMethod("onRewardedVideoShown", arguments: nil)
+    }
+    
+    
+    public   func rewardedVideoWillDismissAndWasFullyWatched(_ wasFullyWatched: Bool) {
+        let args: [String: Any] = [
+            "isFinished": wasFullyWatched
+        ]
+        channel?.invokeMethod("onRewardedVideoClosed", arguments: args)
+    }
+    
+    
+    public  func rewardedVideoDidFinish(_ rewardAmount: Float, name rewardName: String?) {
+        let args: [String: Any] = [
+            "amount": rewardAmount,
+            "reward": rewardName as Any
+        ]
+        channel?.invokeMethod("onRewardedVideoFinished", arguments: args)
+    }
+    
+    
+    public func rewardedVideoDidClick() {
+        channel?.invokeMethod("onRewardedVideoClicked", arguments: nil)
+    }
+    
+    
+    public func rewardedVideoDidExpired(){
+        channel?.invokeMethod("onRewardedVideoExpired", arguments: nil)
+    }
+    
+}
