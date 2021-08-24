272e656741959a5570fd62f6d24062bfd9c24100
KulArtsiom <kulartsiom@gmail.com>
2021-08-24 11:40:00 +0300

    Create AppodealBannerDelegate.swift

---
+++ ios/Classes/AppodealBannerDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppodealBannerDelegate.swift
+//  appodeal_flutter
+//
+//  Created by Artyom Kul on 24.08.21.
+//
+
+import Foundation
+import Appodeal
+
+extension SwiftAppodealFlutterPlugin: AppodealBannerDelegate {
+    
+    public func bannerDidLoadAdIsPrecache(_ precache: Bool) {
+        let args: [String: Any] = [
+            "isPrecache": precache
+        ]
+        channel?.invokeMethod("onBannerLoaded", arguments: args)
+    }
+    
+    public func bannerDidShow() {
+        channel?.invokeMethod("onBannerShown", arguments: nil)
+    }
+    
+    public func bannerDidFailToLoadAd() {
+        channel?.invokeMethod("onBannerFailedToLoad", arguments: nil)
+    }
+    
+    public func bannerDidClick() {
+        channel?.invokeMethod("onBannerClicked", arguments: nil)
+    }
+    
+    public func bannerDidExpired() {
+        channel?.invokeMethod("onBannerExpired", arguments: nil)
+    }
+    
+}
