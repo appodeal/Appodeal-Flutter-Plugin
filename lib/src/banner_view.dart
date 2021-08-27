38cbbd1b18f3774fda53119b6bf7783929d1248e
KulArtsiom <kulartsiom@gmail.com>
2021-08-27 15:38:59 +0300

    Create banner_view.dart

---
+++ lib/src/banner_view.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+class AppodealBannerView extends StatelessWidget {
+  final Map<String, dynamic> _args = {};
+
+  AppodealBannerView({String? placementName}) {
+    this._args["placementName"] = placementName;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 320,
+      height: 50,
+      child: Platform.isIOS
+          ? UiKitView(
+              viewType: 'com.appodeal.appodeal/banner_view',
+              creationParams: this._args,
+              creationParamsCodec: const StandardMessageCodec(),
+            )
+          : AndroidView(
+              viewType: 'com.appoeal.appodeal/banner_view',
+              creationParams: this._args,
+              creationParamsCodec: const StandardMessageCodec(),
+            ),
+    );
+  }
+}
