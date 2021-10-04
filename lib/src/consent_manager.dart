ece3db566e36a36cfa9bcbbdb83d8ffb95ef6e3f
KulArtsiom <kulartsiom@gmail.com>
2021-10-04 13:00:21 +0300

    Update consent_manager.dart

--- lib/src/consent_manager.dart
+++ lib/src/consent_manager.dart
@@ -10,44 +10,59 @@ enum HasConsent { UNKNOWN, TRUE, FALSE }
 enum ShouldShow { UNKNOWN, TRUE, FALSE }
 enum AuthorizationStatus { NOT_DETERMINED, RESTRICTED, DENIED, AUTHORIZED }
 
-class Consent {
-  static const MethodChannel _channel = const MethodChannel('appodeal_flutter');
-
-  late Zone zone;
-  late Status status;
-  late AuthorizationStatus authorizationStatus;
-  late String iabConsentString;
-
-// static Future<HasConsent> hasConsentForVendor(String bundle) async {
-//
-//   _channel.invokeMethod('hasConsentForVendor', {'bundle': bundle});
-// }
-}
-
-class Vendor {
-  String name;
-  String bundle;
-  String policyUrl;
-  List<int> purposeIds;
-  List<int> featureIds;
-  List<int> legitimateInterestPurposeIds;
-
-  Vendor(this.name, this.bundle, this.policyUrl, this.purposeIds, this.featureIds, this.legitimateInterestPurposeIds);
-}
-
 class ConsentManager {
   static const MethodChannel _channel = const MethodChannel('appodeal_flutter');
 
   static Function(String event, String consent)? _onConsentInfoUpdated;
   static Function(String event, String error)? _onFailedToUpdateConsentInfo;
 
-  static Future<void> requestConsentInfoUpdate(String appKey) async {
-    _setConsentInfoUpdateListener();
-    return _channel.invokeMethod('requestConsentInfoUpdate', {'appKey': appKey});
+  static Function(String event)? _onConsentFormLoaded;
+  static Function(String event, String error)? _onConsentFormError;
+  static Function(String event)? _onConsentFormOpened;
+  static Function(String event, String consent)? _onConsentFormClosed;
+
+  static Future<bool> consentFormIsShowing() async {
+    return await _channel.invokeMethod('consentFormIsShowing');
   }
 
-  static Future<void> setCustomVendor(Vendor vendor) async {
-    return _channel.invokeMethod('setCustomVendor', {'name': vendor.name, 'bundle': vendor.bundle, 'policyUrl': vendor.policyUrl, 'purposeIds': vendor.purposeIds, 'featureIds': vendor.featureIds, 'legitimateInterestPurposeIds': vendor.legitimateInterestPurposeIds});
+  static Future<bool> consentFormIsLoaded() async {
+    return await _channel.invokeMethod('consentFormIsLoaded');
+  }
+
+  static Future<void> showAsDialogConsentForm() async {
+    return _channel.invokeMethod('showAsDialogConsentForm');
+  }
+
+  static Future<void> showAsActivityConsentForm() async {
+    return _channel.invokeMethod('showAsActivityConsentForm');
+  }
+
+  static Future<void> loadConsentForm() async {
+    _setConsentFormListener();
+    return _channel.invokeMethod('loadConsentForm');
+  }
+
+  static Future<void> requestConsentInfoUpdate(String appKey) async {
+    _setConsentInfoUpdateListener();
+    return _channel
+        .invokeMethod('requestConsentInfoUpdate', {'appKey': appKey});
+  }
+
+  static Future<void> setCustomVendor(
+      String vendorName,
+      String vendorBundle,
+      String vendorPolicyUrl,
+      var vendorPurposeIds,
+      var vendorFeatureIds,
+      var vendorLegitimateInterestPurposeIds) async {
+    return _channel.invokeMethod('setCustomVendor', {
+      'name': vendorName,
+      'bundle': vendorBundle,
+      'policyUrl': vendorPolicyUrl,
+      'purposeIds': vendorPurposeIds,
+      'featureIds': vendorFeatureIds,
+      'legitimateInterestPurposeIds': vendorLegitimateInterestPurposeIds
+    });
   }
 
   static Future<void> disableAppTrackingTransparencyRequest() async {
@@ -91,7 +106,8 @@ class ConsentManager {
   }
 
   static Future<ShouldShow> shouldShowConsentDialog() async {
-    var getShouldShowType = await _channel.invokeMethod('shouldShowConsentDialog');
+    var getShouldShowType =
+        await _channel.invokeMethod('shouldShowConsentDialog');
     ShouldShow shouldShow = ShouldShow.UNKNOWN;
     switch (getShouldShowType) {
       case 0:
@@ -157,17 +173,45 @@ class ConsentManager {
     return await _channel.invokeMethod('getConsent');
   }
 
+  static void _setConsentFormListener() {
+    _channel.setMethodCallHandler((call) async {
+      if (call.method.startsWith('onConsentFormLoaded')) {
+        _onConsentFormLoaded?.call(call.method);
+      } else if (call.method.startsWith('onConsentFormError')) {
+        _onConsentFormError?.call(call.method, call.arguments['error']);
+      } else if (call.method.startsWith('onConsentFormOpened')) {
+        _onConsentFormOpened?.call(call.method);
+      } else if (call.method.startsWith('onConsentFormClosed')) {
+        _onConsentFormClosed?.call(call.method, call.arguments['consent']);
+      }
+    });
+  }
+
+  static void setConsentFormListener(
+      Function(String event) onConsentFormLoaded,
+      Function(String event, String error) onConsentFormError,
+      Function(String event) onConsentFormOpened,
+      Function(String event, String consent) onConsentFormClosed) {
+    _onConsentFormLoaded = onConsentFormLoaded;
+    _onConsentFormError = onConsentFormError;
+    _onConsentFormOpened = onConsentFormOpened;
+    _onConsentFormClosed = onConsentFormClosed;
+  }
+
   static void _setConsentInfoUpdateListener() {
     _channel.setMethodCallHandler((call) async {
       if (call.method.startsWith('onConsentInfoUpdated')) {
         _onConsentInfoUpdated?.call(call.method, call.arguments['consent']);
       } else if (call.method.startsWith('onFailedToUpdateConsentInfo')) {
-        _onFailedToUpdateConsentInfo?.call(call.method, call.arguments['error']);
+        _onFailedToUpdateConsentInfo?.call(
+            call.method, call.arguments['error']);
       }
     });
   }
 
-  static void setConsentInfoUpdateListener(Function(String event, String consent) onConsentInfoUpdated, Function(String event, String error) onFailedToUpdateConsentInfo) {
+  static void setConsentInfoUpdateListener(
+      Function(String event, String consent) onConsentInfoUpdated,
+      Function(String event, String error) onFailedToUpdateConsentInfo) {
     _onConsentInfoUpdated = onConsentInfoUpdated;
     _onFailedToUpdateConsentInfo = onFailedToUpdateConsentInfo;
   }
