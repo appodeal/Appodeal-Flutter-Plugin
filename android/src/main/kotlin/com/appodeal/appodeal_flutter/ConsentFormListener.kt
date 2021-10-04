5c4b950e7878f831ffd4ecabe6b5da003aef80a9
KulArtsiom <kulartsiom@gmail.com>
2021-10-04 13:00:25 +0300

    Create ConsentFormListener.kt

---
+++ android/src/main/kotlin/com/appodeal/appodeal_flutter/ConsentFormListener.kt
@@ -0,0 +1,29 @@
+package com.appodeal.appodeal_flutter
+
+import com.explorestack.consent.Consent
+import com.explorestack.consent.ConsentFormListener
+import com.explorestack.consent.exception.ConsentManagerException
+import io.flutter.plugin.common.MethodChannel
+
+fun ConsentFormListener(channel: MethodChannel): ConsentFormListener {
+    return object : ConsentFormListener {
+        override fun onConsentFormLoaded() {
+            channel.invokeMethod("onConsentFormLoaded", null)
+        }
+
+        override fun onConsentFormError(error: ConsentManagerException?) {
+            channel.invokeMethod("onConsentFormError",
+                    "consent" to error?.toString())
+
+        }
+
+        override fun onConsentFormOpened() {
+            channel.invokeMethod("onConsentFormOpened", null)
+        }
+
+        override fun onConsentFormClosed(consent: Consent?) {
+            channel.invokeMethod("onConsentFormClosed",
+                    "consent" to consent?.toJSONObject().toString())
+        }
+    }
+}
