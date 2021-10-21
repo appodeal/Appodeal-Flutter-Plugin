dd36813fb39e9f4dcc202a2bc20bf4f8dea1bb5a
KulArtsiom <kulartsiom@gmail.com>
2021-10-21 12:46:47 +0300

    Update AppodealFlutterPlugin.m

--- ios/Classes/AppodealFlutterPlugin.m
+++ ios/Classes/AppodealFlutterPlugin.m
@@ -1,11 +1,11 @@
 #import "AppodealFlutterPlugin.h"
-#if __has_include(<appodeal_flutter/appodeal_flutter-Swift.h>)
-#import <appodeal_flutter/appodeal_flutter-Swift.h>
+#if __has_include(<stack_appodeal_flutter/stack_appodeal_flutter-Swift.h>)
+#import <stack_appodeal_flutter/stack_appodeal_flutter-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "appodeal_flutter-Swift.h"
+#import "stack_appodeal_flutter-Swift.h"
 #endif
 
 @implementation AppodealFlutterPlugin
