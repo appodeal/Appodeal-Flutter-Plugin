e9c36f246661fa217932a616dd218923e87416f8
Denis Glavatskikh <denis.glavatskikh@appodeal.com>
2022-06-08 16:46:53 +0200

    release/3.0.0-beta: prepearing android application;

--- example/lib/ext.dart
+++ example/lib/ext.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
 
 final String appodealKey = Platform.isAndroid
-    ? "fee50c333ff3825fd6ad6d38cff78154de3025546d47a84f"
+    ? "d908f77a97ae0993514bc8edba7e776a36593c77e5f44994"
     : "dee74c5129f53fc629a44a690a02296694e3eef99f2d3a5f";
